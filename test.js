b7697bb2945d4bfb6ec25f89abad1e653cfc23ef
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-03 23:17:00 -0400

    tests for escaping and falsey values

--- test.js
+++ test.js
@@ -27,6 +27,34 @@ describe('hasOwnDeep', function () {
     hasOwnDeep({a: {b: {c: 'd'}}}, 'a.b.c').should.be.true;
   });
 
+  it('should respect keys escaped with slashes when `true` is passed:', function () {
+    hasOwnDeep({'a.b': 'c'}, 'a\\.b', true).should.be.true;
+    hasOwnDeep({'a.b.c': 'd'}, 'a\\.b\\.c', true).should.be.true;
+  });
+
+  it('should not respect keys escaped with slashes when `true` is not passed:', function () {
+    hasOwnDeep({'a.b': 'c'}, 'a\\.b').should.be.false;
+    hasOwnDeep({'a.b.c': 'd'}, 'a\\.b\\.c').should.be.false;
+  });
+
+  it('should work with falsey values:', function () {
+    hasOwnDeep({a: {b: {c: null}}}, 'a').should.be.true;
+    hasOwnDeep({a: {b: {c: null}}}, 'a.b').should.be.true;
+    hasOwnDeep({a: {b: {c: null}}}, 'a.b.c').should.be.true;
+    hasOwnDeep({a: {b: {c: undefined}}}, 'a').should.be.true;
+    hasOwnDeep({a: {b: {c: undefined}}}, 'a.b').should.be.true;
+    hasOwnDeep({a: {b: {c: undefined}}}, 'a.b.c').should.be.true;
+    hasOwnDeep({a: {b: {c: false}}}, 'a').should.be.true;
+    hasOwnDeep({a: {b: {c: false}}}, 'a.b').should.be.true;
+    hasOwnDeep({a: {b: {c: false}}}, 'a.b.c').should.be.true;
+    hasOwnDeep({a: {b: {c: 0}}}, 'a').should.be.true;
+    hasOwnDeep({a: {b: {c: 0}}}, 'a.b').should.be.true;
+    hasOwnDeep({a: {b: {c: 0}}}, 'a.b.c').should.be.true;
+    hasOwnDeep({a: {b: {c: ''}}}, 'a').should.be.true;
+    hasOwnDeep({a: {b: {c: ''}}}, 'a.b').should.be.true;
+    hasOwnDeep({a: {b: {c: ''}}}, 'a.b.c').should.be.true;
+  });
+
   it('should return false when a nested key is not an own property of the given object:', function () {
     hasOwnDeep({a: {b: {c: 'd'}}}, 'c').should.be.false;
     hasOwnDeep({a: {b: {c: 'd'}}}, 'c.c').should.be.false;
